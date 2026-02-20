c1309f7944a0558e736b924303f96252ba62e201
Matt Fishman <mtfishman@users.noreply.github.com>
2026-02-20 10:02:57 -0500

    Catch errors in README generation instead of throwing (#19)

--- src/generate_readme.jl
+++ src/generate_readme.jl
@@ -15,22 +15,28 @@ function generate_readmes!(paths::AbstractVector{<:AbstractString})
 end
 
 function generate_readme!(path::AbstractString)
-    isitensorpkg(path) ||
-        error("Can't generate README: not an ITensor package directory: `$path`.")
-    cd(joinpath(path, "docs")) do
-        julia = Base.julia_cmd()
-        code = """
-        using Pkg: Pkg
-        # Install packages needed for "make_readme.jl".
-        Pkg.instantiate(; io = devnull)
-        include("make_readme.jl")
-        """
-        cmd = `$(julia) --project=. --startup-file=no -e "$(code)"`
-        cmd = setenv(
-            cmd, "JULIA_LOAD_PATH" => "@:@stdlib", "JULIA_PKG_USE_CLI_GIT" => "true"
-        )
-        run(cmd)
+    if !isitensorpkg(path)
+        @warn "Can't generate README: not an ITensor package directory: `$path`."
         return nothing
+    end
+    try
+        cd(joinpath(path, "docs")) do
+            julia = Base.julia_cmd()
+            code = """
+            using Pkg: Pkg
+            # Install packages needed for "make_readme.jl".
+            Pkg.instantiate(; io = devnull)
+            include("make_readme.jl")
+            """
+            cmd = `$(julia) --project=. --startup-file=no -e "$(code)"`
+            cmd = setenv(
+                cmd, "JULIA_LOAD_PATH" => "@:@stdlib", "JULIA_PKG_USE_CLI_GIT" => "true"
+            )
+            run(cmd)
+            return nothing
+        end
+    catch e
+        @warn "Failed to generate README: $e"
     end
     return nothing
 end
