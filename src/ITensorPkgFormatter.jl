d45c0dbeb842befa767b65250af27198dbb387f2
mtfishman <mfishman@flatironinstitute.org>
2026-02-22 22:20:29 -0500

    Suppress printing from make_readme

--- src/ITensorPkgFormatter.jl
+++ src/ITensorPkgFormatter.jl
@@ -1,11 +1,14 @@
 module ITensorPkgFormatter
 
 using ITensorFormatter: ITensorFormatter
+using Logging: Logging
 
 function main(argv)
     ITensorFormatter.main(argv)
     paths = filter(!startswith("--"), argv)
-    ITensorFormatter.generate_readmes!(paths)
+    Logging.with_logger(Logging.NullLogger()) do
+        return ITensorFormatter.generate_readmes!(paths)
+    end
     return nothing
 end
 
